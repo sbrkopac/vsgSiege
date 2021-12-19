1dd4e52005c0435212367ba6b6f6f898f0744380
sbrkopac <sbrkopac@users.noreply.github.com>
2021-12-19 14:03:38 -0700

    added contentdb class but not added to init as it causes things to take a much longer time to load

---
+++ src/ContentDb.hpp
@@ -0,0 +1,26 @@
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include "io/Fuel.hpp"
+
+namespace ehb
+{
+    class IFileSys;
+    class ContentDb final
+    {
+    public:
+        void init(IFileSys& fileSys, const std::string& directory = "/world/contentdb/templates/");
+
+        //! query a string from a given template, for example: "2w_gargoyle:aspect:experience_value"
+        const std::string& queryString(const std::string& query, const std::string& defaultValue = "") const;
+
+        const FuelBlock* getGameObjectTmpl(const std::string& tmpl) const;
+
+    private:
+        std::unordered_map<std::string, std::unique_ptr<FuelBlock>> db;
+    };
+} // namespace ehb

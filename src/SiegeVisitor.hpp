7bbaadd9e03af303f509b8f8289a39e364519344
sbrkopac <sbrkopac@users.noreply.github.com>
2022-01-11 13:49:15 -0700

    messing with vsg visitors

---
+++ src/SiegeVisitor.hpp
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#include <vsg/core/Visitor.h>
+
+namespace ehb
+{
+    class SiegeNodeMesh;
+    class Aspect;
+
+    class SiegeVisitorBase : public vsg::Visitor
+    {
+
+    public:
+
+        virtual void apply(SiegeNodeMesh& node);
+        virtual void apply(Aspect& aspect);
+    };
+}
--- src/world/SiegeNode.hpp
+++ src/world/SiegeNode.hpp
@@ -7,6 +7,8 @@
 #include <vsg/maths/mat4.h>
 #include <vsg/nodes/Group.h>
 #include <vsg/nodes/MatrixTransform.h>
+#include <vsg/core/Visitor.h>
+#include "SiegeVisitor.hpp"
 
 namespace ehb
 {
@@ -21,10 +23,18 @@ namespace ehb
 
         static void connect(const vsg::MatrixTransform* targetRegion, vsg::MatrixTransform* targetNode, uint32_t targetDoor, vsg::MatrixTransform* connectRegion, const vsg::MatrixTransform* connectNode, uint32_t connectDoor);
 
+        void accept(vsg::Visitor& visitor) override;
+
     protected:
         virtual ~SiegeNodeMesh() = default;
 
     private:
         std::vector<std::pair<uint32_t, vsg::dmat4>> doorXform;
     };
+
+    inline void SiegeNodeMesh::accept(vsg::Visitor& visitor)
+    {
+        if(auto v = dynamic_cast<SiegeVisitorBase*> (&visitor))
+            v->apply(*this);
+    }
 } // namespace ehb

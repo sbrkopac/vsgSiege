f13d4583f60d40f9b12a51869b10255f84738117
sbrkopac <sbrkopac@users.noreply.github.com>
2022-06-24 12:51:03 -0600

    added SiegeNodeHeader structure and load it in one-shot

--- src/vsg/ReaderWriterSNO.cpp
+++ src/vsg/ReaderWriterSNO.cpp
@@ -18,6 +18,29 @@ namespace ehb
 {
     static constexpr uint32_t SNO_MAGIC = 0x444F4E53;
 
+    struct SNOHeader
+    {
+        uint32_t id;
+        uint32_t majorVersion;
+        uint32_t minorVersion;
+
+        uint32_t doorCount;
+        uint32_t spotCount;
+
+        uint32_t cornerCount;
+        uint32_t triangleCount;
+
+        uint32_t textureCount;
+
+        vsg::vec3 min;
+        vsg::vec3 max;
+        vsg::vec3 centroidOffset;
+
+        bool tiled;
+
+        uint32_t unk0, unk1, unk2;
+    };
+
     ReaderWriterSNO::ReaderWriterSNO(IFileSys& fileSys) :
         fileSys(fileSys) { log = spdlog::get("log"); }
 
@@ -37,33 +60,21 @@ namespace ehb
 
         // prefer auto since the template provides the type and it's easier to change if we have to
 
-        auto magic = reader.read<uint32_t>();
-        auto version = reader.read<uint32_t>();
-        auto unk1 = reader.read<uint32_t>();
+        auto header = reader.read<SNOHeader>();
 
-        if (magic != SNO_MAGIC) return {};
+        if (header.id != SNO_MAGIC)
+        {
+            log->critical("SNO file has unknown id");
+            return {};
+        }
 
-        auto doorCount = reader.read<uint32_t>();
-        auto spotCount = reader.read<uint32_t>();
-        auto cornerCount = reader.read<uint32_t>();
-        auto faceCount = reader.read<uint32_t>();
-        auto textureCount = reader.read<uint32_t>();
-
-        // SiegeMax calculates a bounding box for us but we will let VSG do some box calculations
-        // This is good for comparatives if we get a "weird" result from VSG
-        auto min = reader.read<vsg::vec3>();
-        auto max = reader.read<vsg::vec3>();
-
-        // No idea what is under here
-        auto unk2 = reader.read<float>(), unk3 = reader.read<float>(), unk4 = reader.read<float>();
-        auto unk5 = reader.read<uint32_t>(), unk6 = reader.read<uint32_t>(), unk7 = reader.read<uint32_t>(), unk8 = reader.read<uint32_t>();
         auto checksum = reader.read<float>();
 
         // Construct our vsg::Group for the graph
         vsg::ref_ptr<SiegeNodeMesh> group = SiegeNodeMesh::create();
 
         // read door data
-        for (uint32_t index = 0; index < doorCount; index++)
+        for (uint32_t index = 0; index < header.doorCount; index++)
         {
             auto id = reader.read<uint32_t>();
 
@@ -100,7 +111,7 @@ namespace ehb
         }
 
         // read spot data
-        for (uint32_t index = 0; index < spotCount; index++)
+        for (uint32_t index = 0; index < header.spotCount; index++)
         {
             // rot, pos, string?
             reader.skipBytes(44);
@@ -108,13 +119,13 @@ namespace ehb
         }
 
         // create vertex data per entire mesh
-        auto vertices = vsg::vec3Array::create(cornerCount);
-        auto normals = vsg::vec3Array::create(cornerCount);
-        auto colors = vsg::vec4Array::create(cornerCount);
-        auto tcoords = vsg::vec2Array::create(cornerCount);
+        auto vertices = vsg::vec3Array::create(header.cornerCount);
+        auto normals = vsg::vec3Array::create(header.cornerCount);
+        auto colors = vsg::vec4Array::create(header.cornerCount);
+        auto tcoords = vsg::vec2Array::create(header.cornerCount);
 
         // read in our vertex data
-        for (uint32_t index = 0; index < cornerCount; index++)
+        for (uint32_t index = 0; index < header.cornerCount; index++)
         {
             reader.readBytes(reinterpret_cast<char*>(&(*vertices)[index]), sizeof(vsg::vec3));
             reader.readBytes(reinterpret_cast<char*>(&(*normals)[index]), sizeof(vsg::vec3));
@@ -125,7 +136,7 @@ namespace ehb
         // Currently for each SiegeNode we create multiple "command graphs" in order to make sure we can pick
         // them apart in the graph for things like fading
         // Not sure if we should be doing this or creating a software representation and just passing 1 draw call to the GPU?
-        for (uint32_t index = 0; index < textureCount; index++)
+        for (uint32_t index = 0; index < header.textureCount; index++)
         {
             auto textureName = reader.readString(); // std::getline(stream, textureName, '\0');
             auto textureFileName = textureName + ".raw";

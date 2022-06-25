51f837696e0fe4ef557b61797e64a6ae606f4ce3
sbrkopac <sbrkopac@users.noreply.github.com>
2022-06-25 14:54:03 -0600

    moving to a visitor

--- src/state/test/BSPTreeTestState.cpp
+++ src/state/test/BSPTreeTestState.cpp
@@ -13,35 +13,6 @@
 
 namespace ehb
 {
-    class BSPTreeRenderer : public SiegeVisitorBase
-    {
-    public:
-
-        int count = 0;
-
-        void apply(vsg::Group& group) override
-        {
-            // this will dispatch to our custom types
-            if (handleCustomGroups(group)) return;
-
-            group.traverse(*this);
-        }
-
-        // we need to handle that region and tell this visitor to traverse it or we'll never get to our nodes
-        void apply(Region& region) override
-        {
-            region.traverse(*this);
-        }
-
-        void apply(SiegeNodeMesh& mesh) override
-        {
-            count++;
-        }
-    };
-}
-
-namespace ehb
-{
     std::string vert_PushConstants2 = R"(#version 450
 #extension GL_ARB_separate_shader_objects : enable
 
@@ -75,6 +46,118 @@ void main() {
 namespace ehb
 {
     vsg::ref_ptr<vsg::Group> bspTriangles; // there is probably a better way to do this
+
+    class BSPTreeRenderer : public SiegeVisitorBase
+    {
+    public:
+
+        int count = 0;
+
+        // Should I utilize SharedObjects and not make this static? There shouldn't be that many instances of this
+        static vsg::ref_ptr<vsg::BindGraphicsPipeline> createGraphicsPipeline()
+        {
+            vsg::ref_ptr<vsg::ShaderStage> vertexShader = vsg::ShaderStage::create(VK_SHADER_STAGE_VERTEX_BIT, "main", vert_PushConstants2);
+            vsg::ref_ptr<vsg::ShaderStage> fragmentShader = vsg::ShaderStage::create(VK_SHADER_STAGE_FRAGMENT_BIT, "main", frag_PushConstants2);
+
+            // set up graphics pipeline
+            vsg::DescriptorSetLayoutBindings descriptorBindings{
+                {0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT, nullptr} // { binding, descriptorTpe, descriptorCount, stageFlags, pImmutableSamplers}
+            };
+
+            vsg::DescriptorSetLayouts descriptorSetLayouts{ vsg::DescriptorSetLayout::create(descriptorBindings) };
+
+            vsg::PushConstantRanges pushConstantRanges{
+                {VK_SHADER_STAGE_VERTEX_BIT, 0, 128} // projection view, and model matrices, actual push constant calls automatically provided by the VSG's DispatchTraversal
+            };
+
+            // TODO: normals
+            vsg::VertexInputState::Bindings vertexBindingsDescriptions{
+                VkVertexInputBindingDescription{0, sizeof(vsg::vec3), VK_VERTEX_INPUT_RATE_VERTEX} // vertex data
+            };
+
+            // TODO: normals
+            vsg::VertexInputState::Attributes vertexAttributeDescriptions{
+                VkVertexInputAttributeDescription{0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0} // vertex data
+            };
+
+            vsg::GraphicsPipelineStates pipelineStates{ vsg::VertexInputState::create(vertexBindingsDescriptions, vertexAttributeDescriptions),
+                                                       vsg::InputAssemblyState::create(),
+                                                       vsg::RasterizationState::create(),
+                                                       vsg::MultisampleState::create(),
+                                                       vsg::ColorBlendState::create(),
+                                                       vsg::DepthStencilState::create() };
+
+            auto pipelineLayout = vsg::PipelineLayout::create(descriptorSetLayouts, pushConstantRanges);
+            auto graphicsPipeline = vsg::GraphicsPipeline::create(pipelineLayout, vsg::ShaderStages{ vertexShader, fragmentShader }, pipelineStates);
+            auto bindGraphicsPipeline = vsg::BindGraphicsPipeline::create(graphicsPipeline);
+
+            return bindGraphicsPipeline;
+        }
+
+        void apply(vsg::Group& group) override
+        {
+            // this will dispatch to our custom types
+            if (handleCustomGroups(group)) return;
+
+            group.traverse(*this);
+        }
+
+        // we need to handle the region and tell this visitor to traverse it or we'll never get to our nodes
+        void apply(Region& region) override
+        {
+            region.traverse(*this);
+        }
+
+        void apply(SiegeNodeMesh& mesh) override
+        {
+            currTree = mesh.tree();
+
+            handleBSPNode(currTree->GetRoot());
+        }
+
+    private:
+
+        BSPTree* currTree;
+
+        void handleBSPNode(BSPNode* node)
+        {
+            if (node->m_IsLeaf)
+            {
+                if (node->m_Triangles)
+                {
+                    for (int32_t i = 0; i < node->m_NumTriangles; ++i)
+                    {
+                        TriNorm& tri = currTree->m_Triangles[node->m_Triangles[i]];
+
+                        auto vertices = vsg::vec3Array::create(3);
+                        auto indices = vsg::ushortArray::create({ 0, 1, 2 });
+
+                        std::memcpy(&(*vertices)[0], &tri.m_Vertices[0], sizeof(vsg::vec3));
+                        std::memcpy(&(*vertices)[1], &tri.m_Vertices[1], sizeof(vsg::vec3));
+                        std::memcpy(&(*vertices)[2], &tri.m_Vertices[2], sizeof(vsg::vec3));
+
+                        auto commands = vsg::Commands::create();
+                        commands->addChild(vsg::BindVertexBuffers::create(0, vsg::DataList{ vertices }));
+                        commands->addChild(vsg::BindIndexBuffer::create(indices));
+                        commands->addChild(vsg::DrawIndexed::create(static_cast<uint32_t>(indices->valueCount()), 1, 0, 0, 0));
+
+                        bspTriangles->addChild(commands);
+
+                        count++;
+                    }
+                }
+            }
+            else
+            {
+                handleBSPNode(node->m_LeftChild);
+                handleBSPNode(node->m_RightChild);
+            }
+        }
+    };
+}
+
+namespace ehb
+{
     void generateBSPDrawRender(BSPTree* tree, const BSPNode* node) // the first param is because BSPTree holds a pointer to all the triangles
     {
         assert(node != nullptr);
@@ -128,44 +211,7 @@ namespace ehb
 
         TimePoint start = Timer::now();
 
-        // set up search paths and load shaders
-        vsg::ref_ptr<vsg::ShaderStage> vertexShader = vsg::ShaderStage::create(VK_SHADER_STAGE_VERTEX_BIT, "main", vert_PushConstants2);
-        vsg::ref_ptr<vsg::ShaderStage> fragmentShader = vsg::ShaderStage::create(VK_SHADER_STAGE_FRAGMENT_BIT, "main", frag_PushConstants2);
-        if (!vertexShader || !fragmentShader) { log->error("Could not create shaders."); }
-
-        // set up graphics pipeline
-        vsg::DescriptorSetLayoutBindings descriptorBindings{
-            {0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT, nullptr} // { binding, descriptorTpe, descriptorCount, stageFlags, pImmutableSamplers}
-        };
-
-        vsg::DescriptorSetLayouts descriptorSetLayouts{ vsg::DescriptorSetLayout::create(descriptorBindings) };
-
-        vsg::PushConstantRanges pushConstantRanges{
-            {VK_SHADER_STAGE_VERTEX_BIT, 0, 128} // projection view, and model matrices, actual push constant calls automatically provided by the VSG's DispatchTraversal
-        };
-
-        // TODO: normals
-        vsg::VertexInputState::Bindings vertexBindingsDescriptions{
-            VkVertexInputBindingDescription{0, sizeof(vsg::vec3), VK_VERTEX_INPUT_RATE_VERTEX} // vertex data
-        };
-
-        // TODO: normals
-        vsg::VertexInputState::Attributes vertexAttributeDescriptions{
-            VkVertexInputAttributeDescription{0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0} // vertex data
-        };
-
-        vsg::GraphicsPipelineStates pipelineStates{ vsg::VertexInputState::create(vertexBindingsDescriptions, vertexAttributeDescriptions),
-                                                   vsg::InputAssemblyState::create(),
-                                                   vsg::RasterizationState::create(),
-                                                   vsg::MultisampleState::create(),
-                                                   vsg::ColorBlendState::create(),
-                                                   vsg::DepthStencilState::create() };
-
-        auto pipelineLayout = vsg::PipelineLayout::create(descriptorSetLayouts, pushConstantRanges);
-        auto graphicsPipeline = vsg::GraphicsPipeline::create(pipelineLayout, vsg::ShaderStages{ vertexShader, fragmentShader }, pipelineStates);
-        auto bindGraphicsPipeline = vsg::BindGraphicsPipeline::create(graphicsPipeline);
-
-#if 0
+#if 1
         if (auto sno = vsg::read_cast<SiegeNodeMesh>(siegeNode, options); sno != nullptr)
         {
             vsg::ref_ptr<vsg::BindGraphicsPipeline> pipeline(options->getObject<vsg::BindGraphicsPipeline>("SiegeNodeGraphicsPipeline"));
@@ -177,16 +223,16 @@ namespace ehb
 
             t1->addChild(sno);
 
-            scene3d.addChild(t1);
+            // scene3d.addChild(t1);
 
-            scene3d.addChild(bindGraphicsPipeline);
+            scene3d.addChild(BSPTreeRenderer::createGraphicsPipeline());
 
-            generateBSPDrawRender(sno->tree(), sno->tree()->GetRoot());
+            // generateBSPDrawRender(sno->tree(), sno->tree()->GetRoot());
 
-            scene3d.addChild(bspTriangles);
-
-            auto count = vsg::visit<BSPTreeRenderer>(&scene3d).count;
+            auto count = vsg::visit<BSPTreeRenderer>(sno).count;
             log->info("COUNT = {}", count);
+
+            scene3d.addChild(bspTriangles);            
 
             // workaround
             compile(systems, systems.scene3d);

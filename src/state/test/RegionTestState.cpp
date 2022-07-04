41e818e3eec749d79a5b0d270b16fa7c88f9cbaa
sbrkopac <sbrkopac@users.noreply.github.com>
2022-07-04 00:52:05 -0600

    added normal drawing for regions

--- src/state/test/RegionTestState.cpp
+++ src/state/test/RegionTestState.cpp
@@ -9,9 +9,155 @@
 
 #include <vsg/io/read.h>
 #include <vsg/maths/transform.h>
+#include <vsg/maths/mat4.h>
 #include <vsg/nodes/MatrixTransform.h>
 #include <vsg/nodes/StateGroup.h>
 #include <vsg/vk/ResourceRequirements.h>
+
+#include <vsg/all.h>
+
+namespace ehb
+{
+    static std::string vert_PushConstants3 = R"(#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(push_constant) uniform PushConstants {
+    mat4 projection;
+    mat4 modelview;
+} pc;
+
+layout(location = 0) in vec3 inPosition;
+
+out gl_PerVertex {
+    vec4 gl_Position;
+};
+
+void main() {
+    gl_Position = (pc.projection * pc.modelview) * vec4(inPosition, 1.0);
+}
+)";
+    static std::string frag_PushConstants3 = R"(#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(location = 0) out vec4 outColor;
+
+void main() {
+    vec3 white = vec3 (1.0, 1.0, 1.0);
+    outColor = vec4(white, 1.0);
+})";
+}
+
+namespace ehb
+{
+    class CreateNormalDebugDataForRegion : public SiegeVisitorBase
+    {
+    public:
+
+        vsg::ref_ptr<vsg::Group> group = vsg::Group::create();
+
+        static vsg::ref_ptr<vsg::BindGraphicsPipeline> createGraphicsPipeline()
+        {
+            vsg::ref_ptr<vsg::ShaderStage> vertexShader = vsg::ShaderStage::create(VK_SHADER_STAGE_VERTEX_BIT, "main", vert_PushConstants3);
+            vsg::ref_ptr<vsg::ShaderStage> fragmentShader = vsg::ShaderStage::create(VK_SHADER_STAGE_FRAGMENT_BIT, "main", frag_PushConstants3);
+
+            vsg::PushConstantRanges pushConstantRanges{
+                {VK_SHADER_STAGE_VERTEX_BIT, 0, 128} // projection view, and model matrices, actual push constant calls automatically provided by the VSG's DispatchTraversal
+            };
+
+            // set up graphics pipeline
+            vsg::DescriptorSetLayoutBindings descriptorBindings{
+                {0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, 1, VK_SHADER_STAGE_FRAGMENT_BIT, nullptr} // { binding, descriptorTpe, descriptorCount, stageFlags, pImmutableSamplers}
+            };
+
+            vsg::DescriptorSetLayouts descriptorSetLayouts{ vsg::DescriptorSetLayout::create(descriptorBindings) };
+
+            vsg::VertexInputState::Bindings vertexBindingsDescriptions{
+                VkVertexInputBindingDescription{0, sizeof(vsg::vec3), VK_VERTEX_INPUT_RATE_VERTEX} // vertex data
+            };
+
+            vsg::VertexInputState::Attributes vertexAttributeDescriptions{
+                VkVertexInputAttributeDescription{0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0} // vertex data
+            };
+
+            vsg::GraphicsPipelineStates pipelineStates{ vsg::VertexInputState::create(vertexBindingsDescriptions, vertexAttributeDescriptions),
+                                           vsg::InputAssemblyState::create(VK_PRIMITIVE_TOPOLOGY_LINE_LIST),
+                                           vsg::RasterizationState::create(),
+                                           vsg::MultisampleState::create(),
+                                           vsg::ColorBlendState::create(),
+                                           vsg::DepthStencilState::create() };
+            auto pipelineLayout = vsg::PipelineLayout::create(descriptorSetLayouts, pushConstantRanges);
+            auto graphicsPipeline = vsg::GraphicsPipeline::create(pipelineLayout, vsg::ShaderStages{ vertexShader, fragmentShader }, pipelineStates);
+            auto bindGraphicsPipeline = vsg::BindGraphicsPipeline::create(graphicsPipeline);
+
+            return bindGraphicsPipeline;
+        }
+
+        void apply(vsg::Group& group) override
+        {
+            if (handleCustomGroups(group)) return;
+
+            group.traverse(*this);
+        }
+
+        void apply(Region& region) override
+        {
+            region.traverse(*this);
+        }
+
+        void apply(vsg::MatrixTransform& transform) override
+        {
+            for (auto&& children : transform.children)
+            {
+                if (auto mesh = children->cast<SiegeNodeMesh>())
+                {
+                    apply(transform.matrix, *mesh);
+                }
+            }
+        }
+
+        void apply(const vsg::dmat4& matrix, SiegeNodeMesh& mesh)
+        {
+            auto log = spdlog::get("log");
+            if (auto renderObject = mesh.renderObject())
+            {
+                // log->info("apply(SiegeNodeMesh&)");
+
+                sVertex norm[2];
+                for (int32_t i = 0; i < renderObject->numVertices(); ++i)
+                {
+                    norm[0] = norm[1] = renderObject->vertices()[i];
+
+                    norm[1].x += mesh.normals()[i].x;
+                    norm[1].y += mesh.normals()[i].y;
+                    norm[1].z += mesh.normals()[i].z;
+
+                    auto vertices = vsg::vec3Array::create(2);
+                    auto indices = vsg::ushortArray::create({ 0, 1 });
+
+                    (*vertices)[0].x = norm[0].x;
+                    (*vertices)[0].y = norm[0].y;
+                    (*vertices)[0].z = norm[0].z;
+
+                    (*vertices)[1].x = norm[1].x;
+                    (*vertices)[1].y = norm[1].y;
+                    (*vertices)[1].z = norm[1].z;
+
+                    for (auto& v : *vertices)
+                    {
+                        v = matrix * (vsg::dvec3) v;
+                    }
+
+                    auto commands = vsg::Commands::create();
+                    commands->addChild(vsg::BindVertexBuffers::create(0, vsg::DataList{ vertices }));
+                    commands->addChild(vsg::BindIndexBuffer::create(indices));
+                    commands->addChild(vsg::DrawIndexed::create(static_cast<uint32_t>(indices->valueCount()), 1, 0, 0, 0));
+
+                    group->addChild(commands);
+                }
+            }
+        }
+    };
+}
 
 namespace ehb
 {
@@ -54,6 +200,11 @@ namespace ehb
 
             SiegeNodeMeshCounter v;
             region->traverse(v);
+
+            scene3d.addChild(CreateNormalDebugDataForRegion::createGraphicsPipeline());
+            CreateNormalDebugDataForRegion debugDataForRegion;
+            region->accept(debugDataForRegion);
+            scene3d.addChild(debugDataForRegion.group);
 
             log->info("Visitor has {} meshes", v.count);
 
